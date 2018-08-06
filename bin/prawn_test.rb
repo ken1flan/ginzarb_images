48b2e15f6243c68c21b957e2d0ebfbe6930fa6f9
ken1flan <ken1flan@gmail.com>
2018-08-06 13:12:37 +0900

    フォントを指定してテキストを出力
    ただ、otfはサポートしてないらしい…。

--- bin/prawn_test.rb
+++ bin/prawn_test.rb
@@ -8,4 +8,12 @@ pdf.text 'Hello, Ginza.rb!'
 # 画像出力
 pdf.image '60th.jpg', width: 150
 
+# フォント指定してテキスト出力
+font_file = '/Users/ken1flan/Library/Fonts/07やさしさゴシックボールド.ttf'
+title = '第60回 RubyKaigi 2018を肴に一杯呑もう！'
+pdf.font(font_file) do
+  pdf.text title
+end
+
+# PDF作成
 pdf.render_file 'hello.pdf'
