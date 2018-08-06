bd3920aab15f6b37d57439ded22edcd1f777891e
ken1flan <ken1flan@gmail.com>
2018-08-07 08:29:12 +0900

    枠線と回転をやってみた

--- bin/prawn_test.rb
+++ bin/prawn_test.rb
@@ -5,14 +5,15 @@ pdf = Prawn::Document.new
 # テキスト出力
 pdf.text 'Hello, Ginza.rb!'
 
-# 画像出力
-pdf.image '60th.jpg', width: 150
-
-# フォント指定してテキスト出力
 font_file = '/Users/ken1flan/Library/Fonts/07やさしさゴシックボールド.ttf'
-title = '第60回 RubyKaigi 2018を肴に一杯呑もう！'
-pdf.font(font_file) do
-  pdf.text title
+pdf.font(font_file) do # フォント指定してテキスト出力
+  title = 'Ginza.rb 第60回 RubyKaigi 2018を肴に一杯呑もう！'
+  image_file = '60th.jpg'
+  pdf.rotate(90, origin: [200, 200]) do # 回転
+    pdf.stroke_rectangle [350, 225], 100, 50 # 枠線
+    pdf.image image_file, width: 150, at: [360, 205] # 画像出力
+    pdf.draw_text title, size: 10, at: [360, 205] # テキスト出力
+  end
 end
 
 # PDF作成
