f91247713628ab41186900118fe7af6cbb9476af
ken1flan <ken1flan@gmail.com>
2018-08-09 19:35:04 +0900

    画像をフィットさせた

--- bin/prawn_test.rb
+++ bin/prawn_test.rb
@@ -12,7 +12,7 @@ pdf.font(font_file) do # フォント指定してテキスト出力
   pdf.rotate(90, origin: [100, 300]) do # 回転
     pdf.bounding_box([100, 300], width: 200, heignt: 200) do
       pdf.stroke_rectangle [0, 0], 200, 200 # 枠線
-      pdf.image image_file, width: 150, at: [0, -20] # 画像出力
+      pdf.image image_file, fit: [200, 200], at: [0, -20] # 画像出力
       # pdf.draw_text title, size: 10, at: [0, 0] # テキスト出力
       pdf.text title, size: 10 # テキスト出力
     end
